e61eceac78f9ad884bb5b19b377075cccbfa5232
saramesc-hub <saramesc@ucm.es>
2026-03-04 19:42:00 +0100

    resumenes añadidos

--- content/works/eyre_content.php/contenido_eyre.php
+++ content/works/eyre_content.php/contenido_eyre.php
@@ -37,80 +37,119 @@
             <section class="menu">
                 <details>
                     <summary><b>Infancia en Gateshead</b></summary>
+                        <p class="pjustificado">En esta etapa inicial conocemos a Jane, una jóven de 10 años. Tras la muerte de sus padres, la niña queda bajo la custodia
+                            de su tía, la señora Reed, la cual la considera una carga y no permite que se integre con la familia por ser una "extraña".</p>
+
+                        <section>
+                            <a class="boton" href="resumenes/resInfanciaGateshead.php">Resúmenes por capítulo</a>
+                        </section>
+
                         <ol>
-                            <a href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap1.php">Capítulo I</a>
-                            <a href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap2.php">Capítulo II</a>
-                            <a href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap2.php">Capítulo III</a>
-                            <a href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap4.php">Capítulo IV</a>
+                            <h4>Capítulos completos</h4>
+                            <a class="boton" href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap1.php">Capítulo I</a>
+                            <a class="boton" href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap2.php">Capítulo II</a>
+                            <a class="boton" href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap2.php">Capítulo III</a>
+                            <a class="boton" href="caps/1.Infancia en Gateshead (Cap. 1–4)/cap4.php">Capítulo IV</a>
                         </ol>
                 </details> 
             </section>
             <section class="menu">
                 <details>
                     <summary><b>Etapa en Lowood</b></summary>
+                        <p class="pjustificado">Lowood es una institución benéfica para huérfanas que, bajo la fachada de caridad cristiana, 
+                            esconde un régimen de privacidad y castigo. Esta etapa representa la lucha por la supervivencia
+                            y el despertar intelectual de Jane</p>
+
+                        <section>
+                            <a class="boton" href="resumenes/resEtapaLowood.php">Resúmenes por capítulo</a>
+                        </section>
+
                         <ol>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap5.php">Capítulo V</a>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap6.php">Capítulo VI</a>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap7.php">Capítulo VII</a>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap8.php">Capítulo VIII</a>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap9.php">Capítulo IX</a>
-                            <a href="caps/2.Etapa en Lowood (Cap. 5–10)/cap10.php">Capítulo X</a>
+                            <h4>Capítulos completos</h4>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap5.php">Capítulo V</a>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap6.php">Capítulo VI</a>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap7.php">Capítulo VII</a>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap8.php">Capítulo VIII</a>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap9.php">Capítulo IX</a>
+                            <a class="boton" href="caps/2.Etapa en Lowood (Cap. 5–10)/cap10.php">Capítulo X</a>
                         </ol>
                 </details>
             </section>
             <section class="menu">
                 <details>
                     <summary><b>Thornfield Hall</b></summary>
+                        <p class="pjustificado">Jane llega a Thornfield con 18 años para convertirse en la institutriz de Adèle Varens.
+                            Lo que parece que iba a ser un empleo tranquilo acaba convirtiéndose en una trama emocional y sobrenatural.</p>
+
+                        <section>
+                            <a class="boton" href="resumenes/resThornfieldHall.php">Resúmenes por capítulo</a>
+                        </section>
+
                         <ol>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap11.php">Capítulo XI</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap12.php">Capítulo XII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap13.php">Capítulo XIII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap14.php">Capítulo XIV</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap15.php">Capítulo XV</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap16.php">Capítulo XVI</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap17.php">Capítulo XVII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap18.php">Capítulo XVIII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap19.php">Capítulo XIX</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap20.php">Capítulo XX</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap21.php">Capítulo XXI</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap22.php">Capítulo XXII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap23.php">Capítulo XXIII</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap24.php">Capítulo XXIV</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap25.php">Capítulo XXV</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap26.php">Capítulo XXVI</a>
-                            <a href="caps/3.Thornfield Hall (Cap. 11–27)/cap27.php">Capítulo XXVII</a>
+                            <h4>Capítulos completos</h4>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap11.php">Capítulo XI</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap12.php">Capítulo XII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap13.php">Capítulo XIII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap14.php">Capítulo XIV</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap15.php">Capítulo XV</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap16.php">Capítulo XVI</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap17.php">Capítulo XVII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap18.php">Capítulo XVIII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap19.php">Capítulo XIX</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap20.php">Capítulo XX</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap21.php">Capítulo XXI</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap22.php">Capítulo XXII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap23.php">Capítulo XXIII</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap24.php">Capítulo XXIV</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap25.php">Capítulo XXV</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap26.php">Capítulo XXVI</a>
+                            <a class="boton" href="caps/3.Thornfield Hall (Cap. 11–27)/cap27.php">Capítulo XXVII</a>
                         </ol>
                 </details>            
             </section>
             <section class="menu">
                 <details>
                     <summary><b>Moor House</b></summary>
+                        <p class="pjustificado">Esta etapa representa una gran prueba para la moral de Jane. Es en este momento en el que debe
+                            decidir quién es. Sin amo. Sin amante. Sin hogar.
+                        </p>
+
+                        <section>
+                            <a class="boton" href="resumenes/resMoorHouse.php">Resúmenes por capítulo</a>
+                        </section>
+
                         <ol>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap28.php">Capítulo XXVIII</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap29.php">Capítulo XXIX</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap30.php">Capítulo XXX</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap31.php">Capítulo XXXI</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap32.php">Capítulo XXXII</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap33.php">Capítulo XXXIII</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap34.php">Capítulo XXXIV</a>
-                            <a href="caps/4.Moor House (Cap. 28–35)/cap35.php">Capítulo XXXV</a>
+                            <h4>Capítulos completos</h4>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap28.php">Capítulo XXVIII</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap29.php">Capítulo XXIX</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap30.php">Capítulo XXX</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap31.php">Capítulo XXXI</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap32.php">Capítulo XXXII</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap33.php">Capítulo XXXIII</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap34.php">Capítulo XXXIV</a>
+                            <a class="boton" href="caps/4.Moor House (Cap. 28–35)/cap35.php">Capítulo XXXV</a>
                         </ol>
                 </details>
             </section>
             <section class="menu">
                 <details>
                     <summary><b>Regreso a Rochester</b></summary>
+                        <p class="pjustificado">Jane y Rochester se reencuentran, pero las circunstancias no son las mismas que la última vez que se vieron.
+                            Thornfield ya no existe, ahora se encuentran en Ferndean, una modesta casita rodeada de bosque. Este
+                            cambio de escenario es esencial, ya que Thornfield era secretos y orgullo, Ferndean representa la curación.</p>
+
+                        <section>
+                            <a class="boton" href="resumenes/resRegresoRochester.php">Resúmenes por capítulo</a>
+                        </section>
+
                         <ol>
-                            <a href="caps/5.Regreso a Rochester (Cap. 36–38)/cap36.php">Capítulo XXXVI</a>
-                            <a href="caps/5.Regreso a Rochester (Cap. 36–38)/cap37.php">Capítulo XXXVII</a>
-                            <a href="caps/5.Regreso a Rochester (Cap. 36–38)/cap38.php">Capítulo XXXVIII</a>
+                            <h4>Capítulos completos</h4>
+                            <a class="boton" href="caps/5.Regreso a Rochester (Cap. 36–38)/cap36.php">Capítulo XXXVI</a>
+                            <a class="boton" href="caps/5.Regreso a Rochester (Cap. 36–38)/cap37.php">Capítulo XXXVII</a>
+                            <a class="boton" href="caps/5.Regreso a Rochester (Cap. 36–38)/cap38.php">Capítulo XXXVIII</a>
                         </ol>
                 </details>
             </section>
-        </section>
-
-        <section>
-            <a class="boton" href="resumenes.php">Resúmenes</a>
         </section>
 
     </section>
